f6a5c8c2c0d664a34f2c932648d56c54ff7bfd05
Brad Cowie <brad@gizmoguy.net.nz>
2013-11-18 15:54:47 +1300

    Move hosts.erb, add puppet header, use clientcert instead of FQDN fact.

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,14 +15,6 @@ class hosts {
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('hosts/etc/hosts.erb'),
-  }
-
-  # Workaround for Puppet 2.7
-  $fqdn_real = inline_template('<%= scope.lookupvar("::fqdn") -%>')
-  if !$fqdn_real {
-    notify { 'fqdn warning':
-      message => '$::fqdn is missing. You may need to fix this by hand.',
-    }
+    content => template('hosts/hosts.erb'),
   }
 }
